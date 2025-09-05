c4ec54b5f2f168606e875d27a588c796c9dfa2a3
ChangYeol96 <wlwhs043@naver.com>
2025-09-05 14:34:50 +0900

    action4

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot! action3";
+        return "Spring Boot! action4";
     }
 
     @GetMapping("/mysql")
